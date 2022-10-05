cd23e7c43844113fb999db58c1f54f67fef8be40
Michael Tracy <michael@Michaels-Air.attlocal.net>
2022-10-05 10:36:33 -0500

    background change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import "./App.css"
 import TodoList from "./components/TodoList";
+import { useState, useEffect} from "react";
+import {db} from './firebase';
+// import { collection, getDocs } from '.firebase/firestore'
 
 function App() {
+  // const [users, setUsers] = useState([]);
+  // const usersCollectionRef = collection(db, 'users')
+  // useEffect(() => {
+  //   const getUsers = async () => {
+  //     const data = await getDocs(usersCollectionRef);
+  //     console.log(data); 
+
+  //   }
+
+
+
+  // }, [])
   return (
+    
     <div className="App">
-      
-     
+   
+      <div>
       <TodoList />
+      </div>
+
      
     </div>
   );
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// import {getFirestore} from "@firebase/firestore";
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBdfvv7_VISV4-6iaiTEX9IgkMkXkh3hNU",
+//     authDomain: "todoproject-7be61.firebaseapp.com",
+//     projectId: "todoproject-7be61",
+//     storageBucket: "todoproject-7be61.appspot.com",
+//     messagingSenderId: "470898107891",
+//     appId: "1:470898107891:web:aab428b248cacc4ac31e86",
+//     measurementId: "G-QLTMX41M1C"
+//   };
+
+//   const app = initializeApp(firebaseConfig);
+//   const analytics = getAnalytics(app);
+
+//   const db = getFirestore
